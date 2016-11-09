58b2872f40b70b6d2c97afdc259e82cc7207e9d6
Przemek 'Smaczny' Porbadnik <przemyslaw.porbadnik.ext@nokia.com>
2016-11-09 16:17:19 +0100

    Implemented container_wrapper at /20-type-template.

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,28 @@
 #pragma once
-
+#include <utility>
+#include <cstddef>
 template<typename T>
-class container_wrapper;
+class container_wrapper{
+public:
+    container_wrapper() = default;
+    
+    container_wrapper(T arg):cont{std::move(arg)}
+    {
+    }
+
+    container_wrapper(const container_wrapper &) = default;
+    container_wrapper(container_wrapper &&) = default;
+
+    container_wrapper & operator=(const container_wrapper &) = default;
+    container_wrapper & operator=(container_wrapper &&) = default;
+
+    std::size_t size() const
+    {
+        return cont.size();
+    }
+    //void operator= (T a){
+    //    this->cont = a;
+    //}    
+    T cont;
+};
 
