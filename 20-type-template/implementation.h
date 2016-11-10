dbdc3d5ff3970b6f312b0669cf0c61b02914932d
Przemek 'Smaczny' Porbadnik <przemyslaw.porbadnik.ext@nokia.com>
2016-11-10 12:02:12 +0100

    Changes after next lecture of templates at NokiaAcademy2016.

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <utility>
 #include <cstddef>
+#include <boost/optional.hpp>
 template<typename T>
 class container_wrapper{
 public:
@@ -19,10 +20,28 @@ public:
     std::size_t size() const
     {
         return cont.size();
-    }
-    //void operator= (T a){
-    //    this->cont = a;
-    //}    
+    }  
     T cont;
 };
+template<typename T>
+class container_wrapper<boost::optional<T>> {
+public:
+    container_wrapper() = default;
+    
+    container_wrapper(boost::optional<T> arg):cont{std::move(arg)}
+    {
+    }
 
+    container_wrapper(const container_wrapper &) = default;
+    container_wrapper(container_wrapper &&) = default;
+
+    container_wrapper & operator=(const container_wrapper &) = default;
+    container_wrapper & operator=(container_wrapper &&) = default;
+
+    std::size_t size() const
+    {
+        return cont ? 1 : 0;
+    }
+
+    boost::optional<T> cont;
+};
