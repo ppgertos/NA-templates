9ee0d91cd1d93ddea277bc33089e75a9cfe695a3
Przemek 'Smaczny' Porbadnik <przemyslaw.porbadnik.ext@nokia.com>
2016-11-10 16:25:57 +0100

    Implementing own algorithms and erase remove idiom.

--- 40-algos-impl/inner_product.h
+++ 40-algos-impl/inner_product.h
@@ -5,6 +5,16 @@ namespace training
 template<typename InputIt1, typename InputIt2, typename T,
     typename BinaryOperation1, typename BinaryOperation2>
 T inner_product(InputIt1 first1, InputIt1 last1, InputIt2 first2, T value,
-    BinaryOperation1 op1, BinaryOperation2 op2);
+    BinaryOperation1 op1, BinaryOperation2 op2){
+        InputIt1 it1 = first1;
+        InputIt2 it2 = first2;
+        while( it1 != last1  )
+        {
+            value = op1(value, op2(*it1,*it2));
+            ++it1;
+            ++it2;
+        }
+        return value;
+    }
 }
 
--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -1,4 +1,9 @@
 #pragma once
-
-// define vector_erase here
-
+#include <algorithm>
+    template <typename T>
+    void vector_erase(T & container,const typename T::value_type & pattern){
+        container.erase(std::remove(
+          container.begin(),
+          container.end(),
+          pattern),container.end());    
+    }
