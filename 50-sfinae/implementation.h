aed77640ddec4552495a951ad80807e74529ae66
Przemek 'Smaczny' Porbadnik <przemyslaw.porbadnik.ext@nokia.com>
2016-11-14 12:04:16 +0100

    Adding some rly HARD sfinae expressions to 50-sfinae/implementation.h and implementation-void_t.h

--- 50-sfinae/implementation.h
+++ 50-sfinae/implementation.h
@@ -1,8 +1,26 @@
 #pragma once
+#include <type_traits>
 
 template<typename T>
-struct has_type_typedef;
+struct has_type_typedef{
+private:
+   // template <typename U, typename = decltype(std::declval<typename U::type>())> mozna tez tak
+    template <typename U, typename = typename U::type>
+    static void test(int);
+    template <typename U>
+    static char test(long);
+public:
+    static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+};
 
 template<typename T>
-struct has_member_variable;
+struct has_member_variable{
+private:
+   template <typename U, typename = decltype(std::declval<U>().member)>
+   static void test(int);
+   template <typename U>
+   static char test(long);
+public:
+   static constexpr bool value = std::is_void<decltype(test<T>(0))>::value;
+};
 
